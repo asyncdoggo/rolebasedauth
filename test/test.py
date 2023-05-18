7d3d16d50f9d3cbfd0c9766f1f2b7619f8b454c2
asyncdoggo <ayushdeshpande81@gmail.com>
2023-05-18 16:29:19 +0530

    Add additional tests

--- test/test.py
+++ test/test.py
@@ -21,6 +21,23 @@ def test_register_admin(username, password, email):
     assert response.status_code == 201
 
 
+def test_register_user(username, password, email):
+    data = {
+        'username': username,
+        'password': password,
+        'email': email
+    }
+    response = requests.post(f"{url}/api/user/register",
+                             json=data,
+                             headers={
+                                 "content-type": "application/json"
+                             }
+                             )
+    print(response.json())
+
+    assert response.status_code == 201
+
+
 def test_get_one_msg_as_admin(msg_id):
     response = requests.get(
         f"{url}/api/messages/{msg_id}",
@@ -64,9 +81,35 @@ def test_get_all_messages_as_admin():
     assert response.status_code == 200
 
 
+def test_login_user(username, password):
+    response = requests.post(
+        f"{url}/api/user/login",
+        auth=(username, password)
+    )
+
+    cookies["user"] = response.cookies
+    assert response.status_code == 200
+
+
+def test_create_msg_as_user(msg):
+    response = requests.post(
+        f"{url}/api/messages",
+        json={
+            "message": msg
+        },
+        cookies=cookies['user']
+    )
+
+    print(response.json())
+    assert response.status_code == 201
+
+
 if __name__ == "__main__":
-    # test_register_admin("user123", "password123##@", "user@example.com")
-    test_login_admin("user123", "password123##@")
+    test_register_admin("admin1234", "password123##@", "admin@example.com")
+    test_login_admin("admin1234", "password123##@")
     test_create_msg_as_admin("hello another msg from admin")
-    test_get_one_msg_as_admin(2)
+    test_get_one_msg_as_admin(1)
     test_get_all_messages_as_admin()
+    test_register_user("user123", "password123##@", "user@example.com")
+    test_login_user("user123", "password123##@")
+    test_create_msg_as_user("Hello, this is another message")
