5fec4d92228e75dff5b73f20d280ef26ca87734e
asyncdoggo <ayushdeshpande81@gmail.com>
2023-05-18 09:33:46 +0530

    Add message table and api routes to test admin and user access

--- app/app.py
+++ app/app.py
@@ -2,17 +2,15 @@ import re
 import uuid
 import flask
 from flask import Flask, session, Blueprint, request, jsonify, make_response
-import app.database as database
+import app.database as db
 
 api_bp = Blueprint('api', __name__)
-
-api_bp.secret_key = ""
 
 email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 password_regex = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{13,}$"
 
 
-@api_bp.route("/api/admin/login")
+@api_bp.route("/api/admin/login", methods=["POST"])
 def admin_login():
     if request.method == "POST":
         try:
@@ -20,7 +18,7 @@ def admin_login():
             if not auth or not auth.username or not auth.password:
                 return make_response(jsonify({'message': 'username or password not present'}))
 
-            user = database.check_auth(auth.username, auth.password)
+            user = db.check_auth(auth.username, auth.password)
             if user:
                 session["user"] = user.id
                 return make_response(jsonify({'message': 'success'}), 200)
@@ -35,9 +33,10 @@ def admin_login():
 @api_bp.route("/api/user/login")
 def user_login():
     pass
+    # TODO: IMPLEMENT TOKEN AUTH FOR USER
 
 
-@api_bp.route("/api/admin/register")
+@api_bp.route("/api/admin/register", methods=["POST"])
 def register(role="admin"):
     if request.method == "POST":
         try:
@@ -48,22 +47,54 @@ def register(role="admin"):
             if not re.search(email_regex, email):
                 return jsonify({'message': 'invalid email format'})
 
-            if len(username) < 10 or " " in username:
+            if len(username) < 6 or " " in username:
                 return make_response(
-                    jsonify({'message': 'username should be greater than 10 characters without spaces'}), 500)
+                    jsonify({'message': 'username should be greater than 6 characters without spaces'}), 500)
 
             if not re.search(password_regex, password):
-                return make_response(jsonify({'message': 'password '}), 500)
+                return make_response(jsonify({'message': 'password should be more than 13 characters with at least a '
+                                                         'number and a special character'}), 500)
 
             user_id = uuid.uuid4().hex
 
-            if database.insert_user(user_id, username, email, password, role):
-                return make_response({jsonify({'message': 'success'})}, 200)
+            if db.insert_user(user_id, username, email, password, role):
+                return make_response(jsonify({'message': 'success'}), 201)
+            else:
+                return make_response(jsonify({'message': 'user or email already registered'}), 500)
 
         except Exception as _:
             return make_response(jsonify({'message': 'incomplete or missing data'}), 404)
 
 
-@api_bp.route("/api/user/register")
+@api_bp.route("/api/user/register", methods=["POST"])
 def user_register():
     register(role="user")
+
+
+@api_bp.route("/api/messages/<msg_id>", methods=["GET", "POST"])
+@api_bp.route("/api/messages", methods=["GET", "POST"])
+def message(msg_id=None):
+    if "user" in session:
+        uid = session["user"]
+
+    else:
+        uid = ""
+        # TODO: check user token return error if neither are present
+
+    if request.method == "GET":
+        msg = db.get_message(uid, msg_id)
+        return make_response(jsonify(msg), 200)
+
+    if request.method == "POST":
+        try:
+            data = request.get_json()
+
+            if "user" in session:
+                uid = session["user"]
+                if db.create_message(uid, data["message"]):
+                    return make_response(jsonify({"message": 'success'}), 201)
+                return make_response(jsonify({"message": "failure"}), 500)
+            return make_response(jsonify({"message": "user not authenticated"}), 403)
+        except Exception as e:
+            print(e)
+            return make_response(jsonify({'message': repr(e)}))
--- app/database.py
+++ app/database.py
@@ -17,10 +17,17 @@ class Users(db.Model):
     role = db.Column(db.String(35), nullable=False)
 
 
+class Message(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.String(35), db.ForeignKey("users.id", ondelete="CASCADE"))
+
+
 def insert_user(user_id, username, email, password, role):
     try:
         hashed = ph.hash(password)
-        user: Users = Users(username=username, id=user_id, password=hashed, role=role)
+        user: Users = Users(username=username, id=user_id, password=hashed, role=role, email=email)
         db.session.add(user)
         db.session.commit()
         return True
@@ -38,3 +45,62 @@ def check_auth(username, password):
         return user
     except Exception as e:
         print(e)
+
+
+def create_message(usr, d):
+    try:
+        data = Message(message=d, user_id=usr)
+        db.session.add(data)
+        db.session.commit()
+        return True
+    except Exception as e:
+        print(e)
+
+
+def get_message(uid, message_id=None):
+    user = Users.query.filter_by(id=uid).one_or_none()
+    msg = []
+    if message_id and user.role == "admin":
+        msg = Message.query.filter_by(id=message_id).one_or_none()
+        if msg:
+            msg = {
+                "id": msg.id,
+                "message": msg.message,
+                "user": msg.user_id
+            }
+    elif message_id and user.role == "user":
+        msg = Message.query.filter_by(id=message_id, user_id=uid).one_or_none()
+        if msg:
+            msg = {
+                "id": msg.id,
+                "message": msg.message,
+                "user": msg.user_id
+            }
+    elif not message_id and user.role == "admin":
+        data = Message.query.all()
+        if data:
+            for i in data:
+                msg.append(
+                    {
+                        "id": i.id,
+                        "message": i.message,
+                        "user": i.user_id
+                    }
+                )
+    elif not message_id and user.role == "user":
+        data = Message.query.filter_by(user_id=uid).all()
+        if data:
+            for i in data:
+                msg.append(
+                    {
+                        "id": i.id,
+                        "message": i.message,
+                        "user": i.user_id
+                    }
+                )
+
+    return msg
+
+
+def get_user(uid):
+    return Users.query.filter_by(id=uid).one_or_none()
