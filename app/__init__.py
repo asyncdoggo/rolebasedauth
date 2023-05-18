0b39129f4884607cdc03a96249444d3e3348392b
asyncdoggo <ayushdeshpande81@gmail.com>
2023-05-18 09:32:55 +0530

    Make session expiry to 30 minutes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
+import flask
 from flask import Flask
-import app.database as Data
+import app.database as database
 from app.app import api_bp
 
 app = Flask(__name__)
@@ -7,8 +10,13 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 with app.app_context():
-    Data.db.init_app(app)
-    Data.db.create_all()
-
+    database.db.init_app(app)
+    database.db.create_all()
 
 app.register_blueprint(api_bp)
+
+
+@app.before_request
+def make__session_permanent():
+    flask.session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=30)
